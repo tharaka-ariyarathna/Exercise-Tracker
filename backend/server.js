817b562811685e8af4d05713ae9e4b6dc5c2f50a
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-06-19 12:03:06 +0530

    Created models for database

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express') ;
+const cors = require('cors') ;
+const mongoose = require('mongoose') ;
+const { queryAllByPlaceholderText } = require('@testing-library/react');
+
+require('dotenv').config() ;
+
+const app = express() ;
+const port = process.env.port || 5000 ;
+
+app.use(cors()) ;
+app.use(express.json()) ;
+
+const uri = process.env.ATLAS_URI ;
+
+mongoose.connect(uri, {}
+    ) ;
+
+const connection = mongoose.connection ;
+connection.once('open', () => {
+    console.log("MongoDB connection established successfully") ;
+})
+
+app.listen(port, () => {
+    console.log(`Server is runningon port : ${port}`) ;
+})
