02d97140baea594d56d176cf766033d095e56348
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-07-01 15:51:51 +0530

    Created navigation in frontend

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react' ;
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+
+import Navbar from './Components/Navbar' ;
+import ExerciseList from './Components/ExerciseList.component' ;
+import EditExercise from './Components/EditExercise.component' ;
+import CreateExercise from './Components/CreateExercise.component' ;
+import CreateUser from './Components/CreateUser.component' ;
+
+const App = () => {
+    return(
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<ExerciseList />} />
+                <Route path="/edit/:id" element={<EditExercise />} />
+                <Route path="/create" element={<CreateExercise />} />
+                <Route path="/user" element={<CreateUser />} />
+            </Routes>
+        </Router>
+    ) ;
+}
+
+export default App ;
