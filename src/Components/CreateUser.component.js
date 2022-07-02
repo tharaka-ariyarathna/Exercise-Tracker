0ac2a7b94a71191fe872fdd83920c68b84b3d255
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-07-03 01:24:03 +0530

    Completed CreateUSer component

--- src/Components/CreateUser.component.js
+++ src/Components/CreateUser.component.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateUser = () => {
+    const [username, setUsername] = useState('') ;
+
+    const onUsernameChange = (e) => {
+        setUsername(e.target.value) ;
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault() ;
+        const user = {
+            username : username
+        }
+        console.log(user) ;
+
+        setUsername('') ;
+    }
+
     return (
-        <div>This is CreateUser component</div>
+        <div>
+            <h3>Create New User</h3>
+            <form onSubmit={onFormSubmit}>
+                <div className='form-group row my-3'>
+                    <label className='col-sm-2 col-form-label'>Username</label>
+                    <div className='col-sm-6'>
+                        <input
+                            type='text'
+                            required
+                            className='form-control'
+                            value={username}
+                            onChange={onUsernameChange}
+                        />
+                    </div>
+                </div>
+                <div className='form-group my-3 mx-4'>
+                    <input type='submit' className='btn btn-primary' value='Create User'/>
+                </div>
+            </form>
+        </div>
     );
 }
 
