9ad45a784a6183d27dba0277edd2b6f87a60bc88
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-07-03 01:53:46 +0530

    Connected frontend and backend

--- src/Components/CreateUser.component.js
+++ src/Components/CreateUser.component.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const CreateUser = () => {
     const [username, setUsername] = useState('') ;
@@ -12,7 +13,8 @@ const CreateUser = () => {
         const user = {
             username : username
         }
-        console.log(user) ;
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data)) ;
 
         setUsername('') ;
     }
